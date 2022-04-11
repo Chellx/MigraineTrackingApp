508b9858eaccc45e683ee2157db2563202ba7aa4
Michelle Bolger <michellebolgerlaois@gmail.com>
2022-04-11 19:51:44 +0100

    changed icon and app name

--- MigraineTrackingApp.Android/MainActivity.cs
+++ MigraineTrackingApp.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Firebase;
 
 namespace MigraineTrackingApp.Droid
 {
-    [Activity(Label = "MigraineTrackingApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "migraineHub", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
