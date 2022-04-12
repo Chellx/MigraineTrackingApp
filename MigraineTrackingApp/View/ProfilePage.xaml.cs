b26c406ebe7c7555e883ddb036774bcffe2b3cd9
Michelle Bolger <michellebolgerlaois@gmail.com>
2022-04-12 16:29:20 +0100

    Added radio buttons for gender and date picker for DOB

--- MigraineTrackingApp/View/ProfilePage.xaml.cs
+++ MigraineTrackingApp/View/ProfilePage.xaml.cs
@@ -16,6 +16,8 @@ namespace MigraineTrackingApp
         Stream stream;
         string fileName;
         string Id = "";
+        private RadioButton button;
+        private string gender = "";
         public ProfilePage(string userId)
         {
             InitializeComponent();
@@ -36,32 +38,39 @@ namespace MigraineTrackingApp
             {
                 memberFirstName.Text = member.FirstName;
                 memberLastName.Text = member.LastName;
-                memberGender.Text = member.Gender;
-                memberDob.Text = member.Dob;
+               // memberDob.Text = member.Dob;
             }
         }
-        async void Button_Clicked(System.Object sender, System.EventArgs e)
-        {
-            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
-            {
-                Title = "Please pick a photo"
-            });
 
-            if (result != null)
-            {
-                stream = await result.OpenReadAsync();
-                fileName = result.FileName;//returns pic name
-               
-                resultImage.Source = ImageSource.FromStream(() => stream);
-                
-                //Image image = ImageSource.FromStream(() => stream);
-            }
+        private void getGender(object sender, CheckedChangedEventArgs e)
+        {
+            button = sender as RadioButton;
+            gender = button.Content.ToString();
         }
+
+
+        /*  async void Button_Clicked(System.Object sender, System.EventArgs e)
+          {
+              var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+              {
+                  Title = "Please pick a photo"
+              });
+
+              if (result != null)
+              {
+                  stream = await result.OpenReadAsync();
+                  fileName = result.FileName;//returns pic name
+
+                  resultImage.Source = ImageSource.FromStream(() => stream);
+
+                  //Image image = ImageSource.FromStream(() => stream);
+              }
+          }*/
         async void updateInfo(System.Object sender, System.EventArgs e)
         {
-            memberVm.createProfile(memberFirstName.Text, memberLastName.Text, memberDob.Text, memberGender.Text, Id, fileName);
+           memberVm.createProfile(memberFirstName.Text, memberLastName.Text, memberDob.Date.ToString(), gender,Id, fileName);
             
-            memberVm.uploadPhoto(stream,fileName);
+           // memberVm.uploadPhoto(stream,fileName);
         }
     }
 }
